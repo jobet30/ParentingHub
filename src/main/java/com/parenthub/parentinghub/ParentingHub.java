b9884e6fca17e6c6f2ddd66f207c6a4e38a5745a
jobet1995 <jobetcasquejo221@gmail.com>
2024-10-23 21:07:27 +0800

    initialize

---
+++ src/main/java/com/parenthub/parentinghub/ParentingHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.parenthub.parentinghub;
+
+/**
+ *
+ * @author Jobet Casquejo
+ */
+public class ParentingHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
